36a09fb51bff818ad122b36c4d42a26a237df58b
shafi <shafi.tokhi@gmail.com>
2012-12-06 18:20:16 +0430

    reverse sentence via stack

---
+++ SentenceReverse.java
@@ -0,0 +1,38 @@
+package deque;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class SentenceReverse {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Deque<String> stack = new ArrayDeque<String>();
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter a sentence (e.g; The quick brown fox jumped): ");
+		String sen = scan.nextLine();
+		
+		String[] words = tokenize(sen);
+		
+		for(int i = 0 ; i < words.length; i++){
+			stack.push(words[i]);
+		}
+		
+		System.out.println(stack);
+		
+
+	}
+	
+	public static String[] tokenize(String sentence){
+		String[] words = null;
+		for (int i = 0; i < sentence.length(); i++) {
+				words = sentence.split(" ");
+		}
+		return words;
+	}
+	
+}
